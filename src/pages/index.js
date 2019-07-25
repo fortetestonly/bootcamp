41ffdc1db41f6dd84a06313f3a9d26a9a25bd77c
fortetestonly <43376028+fortetestonly@users.noreply.github.com>
2019-07-25 16:18:05 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Footer from '../components/footer'
 const IndexPage = () => {
@@ -6,7 +7,7 @@ const IndexPage = () => {
     <Layout>
       <h1>Hello</h1>
       <h2>My Name is Gurpreet Kaur</h2>
-      <p>Need Developer <a href="/about">About Us</a></p>
+      <p>Need Developer <Link to="/about">About Us</Link></p>
       <Footer />
     </Layout>
   )

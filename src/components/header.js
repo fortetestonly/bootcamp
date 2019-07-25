ea26e17b314810a1dbda46c363fa033442ebfb84
fortetestonly <43376028+fortetestonly@users.noreply.github.com>
2019-07-25 16:13:55 +0530

    Add files via upload

--- src/components/header.js
+++ src/components/header.js
@@ -27,6 +27,11 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <ul>
+      	<li><Link to="/">Home</Link></li>
+      	<li><Link to="/about">About</Link></li>
+      	<li><Link to="/contact">Contact</Link></li>
+      </ul>
     </div>
   </header>
 )
